cec45a19c0e4f8ca990e063fb210a50d1c76ef37
jystella17 <42269221+jystella17@users.noreply.github.com>
2022-08-01 23:58:28 +0900

    Create 가장 긴 팰린드롬.cpp

---
+++ Implementation/가장 긴 팰린드롬.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+int solution(string s)
+{
+    int answer=0;
+    int size = s.length();
+    
+    for(int len=size; len>1; len--){
+        for(int start=0; start<=size - len; start++){
+            bool isPalindrome = true;
+            
+            for(int i=0; i<len/2; i++){
+                if(s[start+i] != s[start+size-i-1]){
+                    isPalindrome = false;
+                    break;
+                }
+            }
+            
+            if(isPalindrome)
+                return len;
+        }
+    }
+}
