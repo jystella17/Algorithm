946f2f7142487aedcace0da3251b6df89b2af86c
jystella17 <42269221+jystella17@users.noreply.github.com>
2023-04-03 21:17:17 +0900

    Update BOJ 2565.cpp

--- Dynamic Programming/BOJ 2565.cpp
+++ Dynamic Programming/BOJ 2565.cpp
@@ -1,36 +1,35 @@
-// 전깃줄 - 최대/최장 증가 수열 (LIS, Longest Increasing Subsequence)
+// BOJ G5 전깃줄
+// 최대/최장 증가 수열 (LIS, Longest Increasing Subsequence)
 
 #include <iostream>
-#include <algorithm>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 int main(){
     vector<pair<int, int>> electric;
-    int dp[101] = {0, };
-    int n;
-    int ans = 0;
-    cin>>n;
+    int dp[101] = {0, }; 
+    int num, answer = 0; 
+    cin>>num;
     
-    electric.push_back(make_pair(0,0));
-    for(int i=0; i<n; i++){
-        int tx, ty;
-        cin>>tx>>ty;
-        electric.push_back(make_pair(tx,ty));
+    electric.push_back(make_pair(0, 0));
+    for(int i=0; i<num; i++){
+        int start, dest;
+        cin>>start>>dest;
+        electric.push_back(make_pair(start, dest));
     }
     
     sort(electric.begin(), electric.end());
-
-    for(int i=1; i<=n; i++){
+    
+    for(int i=1; i<=num; i++){
+        dp[i] = 1;
         for(int j=0; j<i; j++){
-            if(electric[i].second > electric[j].second){
+            if(electric[i].second > electric[j].second)
                 dp[i] = max(dp[j]+1, dp[i]);
-            }
         }
-        ans = max(ans, dp[i]);
+        answer = max(answer, dp[i]);
     }
     
-    cout<<n - ans<<endl;
-
+    cout<<num - answer<<"\n";
     return 0;
 }
