b8847cc7b65f17aeea684b7f112e6f9676e25133
jystella17 <42269221+jystella17@users.noreply.github.com>
2023-05-08 21:25:11 +0900

    Update BOJ 1339.py

--- Greedy/BOJ 1339.py
+++ Greedy/BOJ 1339.py
@@ -1,13 +1,11 @@
 import sys
 
-
 n = int(sys.stdin.readline())
 word = []
 alpha_weight = {'A':0, 'B':0, 'C':0, 'D':0, 'E':0, 'F':0, 'G':0, 'H':0, 'I':0, 
                 'J':0, 'K':0, 'L':0, 'M':0, 'N':0, 'O':0, 'P':0, 'Q':0, 'R':0, 
                 'S':0, 'T':0, 'U':0, 'V':0, 'W':0, 'X':0, 'Y':0, 'Z':0}
-alpha_num = []
-ans = 0
+alpha_num, ans = [], 0
 
 for i in range(n):
     word.append(sys.stdin.readline().rstrip())
